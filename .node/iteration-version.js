3cbf4c89054a79c875c10834020d5321a687c5d2
YAYALUOYA <yayaluoya@sina.com>
2025-10-31 08:56:00 +0800

    feat: 指定主版本和次版本，自动迭代修订版本号

--- .node/iteration-version.js
+++ .node/iteration-version.js
@@ -12,12 +12,19 @@ const __dirname = path.dirname(__filename);
 /**
  * @typedef {Object} IterationVersionOptions
  * @property {string} [major] 主版本号
+ * @property {string} [minor] 次版本号
+ */
+
+/**
+ * 迭代版本号
+ * 指定主版本号和次版本号，自动迭代修订版本号
  */
 
 program
   .version("1.0.0")
   .description("迭代版本号")
   .option("-m, --major <major>", "主版本号")
+  .option("-n, --minor <minor>", "次版本号")
   .action(() => {
     iterationVersion(program.opts());
   })
@@ -27,13 +34,13 @@ program
  * @param {IterationVersionOptions} options
  */
 async function iterationVersion(options) {
-  const { major = "1" } = options;
+  const { major = "1", minor = "0" } = options;
 
   const packageJsonPath = path.join(__dirname, "../package.json");
 
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
 
-  const newVersion = `${major}.${dayjs().format("YYYYMMDD")}.${dayjs().format("HHmmss")}`;
+  const newVersion = `${major}.${minor}.${dayjs().format("YYYYMMDDHHmmss")}`;
 
   console.log(`${packageJson.name} 的版本号迭代为 ${newVersion}`);
 
